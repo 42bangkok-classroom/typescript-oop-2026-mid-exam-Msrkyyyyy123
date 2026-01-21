0ceedb19f7edbaec33c245939ec489b2b1f35006
anwuth <anuwuth.ch@gmail.com>
2026-01-21 15:24:24 +0700

    update

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -3,7 +3,7 @@ const inputNumber = process.argv[2];
 const point = Number(inputNumber);
 
 if(Number.isNaN(point) || inputNumber === ''){
-     
+    // process.exit();
 } else { 
     for(let i = 1; i<=point; i++) {
     if (i % 3 == 0 && i % 5 == 0){
