30d994d367f34ce2a865dc2946994b4f190426d0
anwuth <anuwuth.ch@gmail.com>
2026-01-21 14:58:30 +0700

    update

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -3,8 +3,7 @@ let points = (process.argv[2])
 let Input1 = Number(points);
 
 if(Number.isNaN(Input1)||(Input1 < 0) || (Input1 > 100) || points === '' || !points ){
-   console.log("Invalid Input");
-   process.exit();  
+   console.log("Invalid Input"); 
 }
 
 if (Input1 < 50) {
